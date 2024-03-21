b3f634f979528e2203040f172af144888415975b
natebuch <nate.buch@gmail.com>
2024-03-21 15:58:16 -0500

    New endpoint for full data object

---
+++ pages/api/stx-transfer-chainhook copy.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { createClient } from '@supabase/supabase-js'
+ 
+type ResponseData = {
+  message: string
+}
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_KEY as string);
+
+function epochToJsDate(timestamp: any){
+  return new Date(timestamp*1000).toISOString();
+}
+
+async function insertData( req_data: any ) {
+  const { data, error } = await supabase
+        .from('transactions')
+        .insert([
+          { 
+            transaction_data: req_data
+          }
+        ])
+        .select()
+  if (error) {
+    console.error('Error inserting data:', error);
+    return;
+  }  
+  console.log(data)
+}
+ 
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) 
+{
+  if (req.method !== 'POST') {
+      res.status(405).send({ message: 'Only POST requests allowed' })
+      return
+    }
+  {
+    insertData(req.body)
+    res.status(200).json({ message: 'Hello from Next.js!' })
+  }
+}
+
